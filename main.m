07d38310ca6979e9896809c019760d4a9d9ce631
Joanna1212 <527860316@qq.com>
2017-10-17 19:07:45 +0800

    push

--- main.m
+++ main.m
@@ -3,13 +3,11 @@ clc
 clear
 close all
 
-% filename = 'testSet.txt';
-K = 10;
-% data = load(filename);
-data = csvread('C:\\Users\MIRJYL\Desktop\X1_test.csv');
+%Read the file
+filename = 'testSet.txt';
+data = load(filename);
+%data = csvread('C:\\Users\MIRJYL\Desktop\X1_test.csv');
+
 [lowData,reconMat] = PCA(data,K);
 
-% figure
-% scatter(lowData(:,1),lowData(:,2),5,'r');
-% % save('C:\\Users\MIRJYL\Desktop\X.csv',lowData);
-csvwrite('C:\\Users\MIRJYL\Desktop\X.csv',lowData);
+csvwrite('X.csv',lowData);
